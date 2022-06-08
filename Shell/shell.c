53da3bae5c19a5bd4afaa65293125835df042743
Saishivareddy <saishivareddy75@gmail.com>
2022-06-08 22:53:23 +0000

    Dummy Shell

---
+++ Shell/shell.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    pid_t id;
+    unsigned char cmd[100];
+    printf("CDAC Shell ##: ");
+    scanf("%s",cmd);
+    id =fork();
+    if(id==0)
+    {
+        execlp(cmd,cmd,NULL);
+    }
+    else
+    {    
+        wait(NULL);
+    }
+    return 0;
+}
